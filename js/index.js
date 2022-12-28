0b4f003c255ae5e42d1c0d1fab182af24d5ce147
Alex de Campos Santos Toledo Camilo <alexcs@outlook.com.br>
2022-12-28 16:21:07 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
-$(document).ready(function(){
-    $(".menu").click(function(){
-        $(".keep").toggleClass("width");
+let inputs = document.getElementsByClassName('input-form');
+for (let input of inputs) {
+    input.addEventListener("blur", function() {
+        if(input.value.trim() != ""){
+            input.classList.add("has-val");
+        } else {
+            input.classList.remove("has-val");
+        }
     });
-});
+}
