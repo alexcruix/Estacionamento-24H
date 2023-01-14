49b70159544ececa9619f30f48db611d4b6d46e7
Alex de Campos Santos Toledo Camilo <alexcs@outlook.com.br>
2023-01-14 09:39:28 -0300

    Create inicial.js

---
+++ js/inicial.js
@@ -0,0 +1,22 @@
+if (localStorage.getItem("token") == null){
+    alert("Você precisa estar logado para acessar essa página");
+    window.location.href = "../Estacionamento-24H/index.html";
+}
+
+let userLogado = JSON.parse(localStorange.getItem("userLogado"));
+
+let logado = document.querySelector("logado");
+logado.innerHTML = 'Olá ${userLogado.nome}';
+
+function sair() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLogado");
+    window.location.href = "../Estacionamento-24H/index.html";
+}
+
+
+$(document).ready(function(){
+    $(".menu").click(function(){
+        $(".keep").toggleClass("width");
+    });
+});
